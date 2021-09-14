edeee28109f0c5ffdb877ff9f303533a33a97c7e
TruPotato <trupotatoes@outlook.com>
2021-09-13 23:52:25 -0700

    Finished loading and error screens as well as made a small 'site' for convenience

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   
   return (
     <div className='container'>
+      <h1>Hi!</h1>
+      <p>It's very late, and, due to my brain's ongoing campaign to convince me that I'm worthless, I haven't gotten much done. In an attempt to finish this assignment on time, I've made this very simple site- It has several buttons to trigger loading and errors, for convenience.</p><br/>
       <Setup />
     </div>
   )
--- src/Components/LoadingFunc.js
+++ src/Components/LoadingFunc.js
@@ -1,26 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading'
 import Error from './Error';
-const url = 'https://api.github.com/users/QuincyLarson';
+const url = 'https://api.github.com/users/TruPotato';
 const MultipleReturns = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState('defaultUser');
+  const [avatar, setAvatar] = useState('defaultAvatar');
 
   useEffect(()=>{
     fetch(url)
     .then((response)=>response.json())
     .then((user)=>{
-      const {login} = user
+      const {login, avatar_url} = user 
       setUser(login);
+      setAvatar(avatar_url);
       setIsLoading(false);
-      setIsError(true);
     })
     .catch((error)=>{
       console.log(error)
       (error) ? setIsError(true) : setIsError(false);
     });
-  })
+  }, []);
 
   if (isError) {
     return (
@@ -40,6 +41,10 @@ const MultipleReturns = () => {
 
   return <div>
     <h1>{user}</h1>
+    <img className="rounded" src={avatar} alt="it's me!" />
+    <p>Hey, it worked!</p>
+    <button onClick={() => {setIsError(true)}}>Press this button to error out.</button><br/>
+    <button onClick={() => {setIsLoading(true)}}>Press this button to load indefinitely.</button>
   </div>;
 };
 
